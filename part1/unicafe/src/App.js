a0a040ac11cc9755c02fd9712ed0460fc7cd2ca7
galyfilsaime <filsaimegaly@gmail.com>
2021-05-10 22:55:35 -0400

    1.11*: unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,26 +6,51 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Statistic = ({text, value}) => {
+  return (
+  <p>{text} {value}</p>
+  )
+}
+
 const Statistics = ({good, neutral, bad, average, positive}) => {
   if (good === 0 && bad === 0 && neutral === 0) {
     return (
       <div>
-        <h1>statistics</h1>
         <p>no feedback provided</p>
       </div>
     )
   }
   return (
   <div>
-    <h1>statistics</h1>
-    <p>
-      good {good}<br/>
-      neutral {neutral}<br/>
-      bad {bad} <br/>
-      total {good + neutral + bad} <br/>
-      average {average} <br/>
-      positive {positive} %
-    </p>
+    <table>
+      <tbody>
+        <tr>
+          <td><Statistic text='good' /></td>
+          <td><Statistic value={good} /></td>
+        </tr>
+        <tr>
+          <td><Statistic text='neutral' /></td>
+          <td><Statistic value={neutral} /></td>
+        </tr>
+        <tr>
+          <td><Statistic text='bad' /></td>
+          <td><Statistic value={bad} /></td>
+        </tr>
+        <tr>
+          <td><Statistic text='total' /></td>
+          <td><Statistic value={good + neutral + bad} /></td>
+        </tr>
+        <tr>
+          <td><Statistic text='average' /></td>
+          <td><Statistic value={average} /></td>
+        </tr>
+        <tr>
+          <td><Statistic text='positive' /></td>
+          <td><Statistic value={positive}/></td>
+          <td>%</td>
+        </tr>
+      </tbody>
+    </table>
   </div> )
 }
 
@@ -46,6 +71,7 @@ const App = () => {
         <Button handleClick = {()=>setNeutral(neutral+1)} text='neutral'/>
         <Button handleClick = {()=>setBad(bad+1)} text='bad'/>
       </div>
+      <h1>statistics</h1>
       <Statistics
         good = {good}
         neutral = {neutral}
